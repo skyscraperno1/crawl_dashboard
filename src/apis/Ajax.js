2f719076cb212a3df5254762acd80598df583f94
Simon <896073330@qq.com>
2024-04-18 12:00:36 +0800

    baseURl

--- src/apis/Ajax.js
+++ src/apis/Ajax.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const Axios = axios.create({
   // baseURL: '/api',
-  baseURL: 'http://' + window.location.hostname + ':9011',
+  // baseURL: 'http://' + window.location.hostname + ':9011',
+  baseURL: 'http://http://192.168.110.50:9011',
   timeout: 10000
 })
 
